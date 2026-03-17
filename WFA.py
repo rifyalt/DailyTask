0c8c216016e58dde02cacc9ee51b537ca46cd0e3
vial <60603588+rifyalt@users.noreply.github.com>
2026-03-17 16:05:14 +0700

    Update WFA.py

--- WFA.py
+++ WFA.py
@@ -293,7 +293,7 @@ WORK_HOURS = [f"{h:02d}:00" for h in range(0, 25)]  # 00:00–24:00
 
 CATEGORY_LIST = [
     "Booking","Voucher Issued","Follow Up Hotel","Follow Up Supplier",
-    "Void","Refund","Rename Guest","Takeover Payment",
+    "Void","Refund","Rename Guest","Takeover Payment","Online Meeting",
     "Inject Debit DTM","Complaint Handling","Rekap Tagihan",
 ]
 DETAIL_LIST = sorted([
